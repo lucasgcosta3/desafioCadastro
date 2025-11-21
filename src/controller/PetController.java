d4d36d93af9d56279d8127985c88dc1847436e32
Lucas Costa <lg1352439@gmail.com>
2025-11-21 17:47:51 -0300

    feat: adiciona os métodos list e search ao controller (passo 5)

--- src/controller/PetController.java
+++ src/controller/PetController.java
@@ -2,18 +2,53 @@ package controller;
 
 import model.Pet;
 import service.FileService;
-import service.PetService;
+import service.PetRegister;
+import service.PetSearch;
 
+import java.util.List;
 import java.util.Scanner;
 
+import static util.MenuUtil.showSearchMenu;
+
 public class PetController {
-    private final PetService petService = new PetService();
+    private final PetRegister petRegister = new PetRegister();
+    private final PetSearch petSearch = new PetSearch();
     private final FileService fileService = new FileService();
 
     public void register(Scanner sc) {
         System.out.println("--- Cadastrar Pet ---");
-        Pet pet = petService.registerPet(sc);
+        Pet pet = petRegister.registerPet(sc);
         fileService.savePetFile(pet);
         System.out.println("Pet cadastrado e salvo com sucesso!");
+    }
+
+    public void list() {
+        List<Pet> pets = fileService.readPetFile();
+        if (!pets.isEmpty()) {
+            int i = 1;
+            for (Pet pet : pets) {
+                System.out.println(i++ + ". " + pet);
+            }
+        } else {
+            System.out.println("Nenhum pet cadastrado.");
+        }
+    }
+
+    public void search(Scanner sc) {
+        System.out.println("--- Buscar Pets ---");
+
+        List<String> filters = showSearchMenu(sc);
+        List<Pet> pets = fileService.readPetFile();
+
+        System.out.println("--- Resultados ---");
+        List<Pet> results = petSearch.searchPets(filters, pets);
+        if (!results.isEmpty()) {
+            int i = 1;
+            for (Pet pet : results) {
+                System.out.println(i++ + ". " + pet);
+            }
+        } else {
+            System.out.println("Nenhum pet encontrado.");
+        }
     }
 }
