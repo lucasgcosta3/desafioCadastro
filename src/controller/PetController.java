3ea42893a9a6f7d0852b051c28f93a13f9d80dc3
Lucas Costa <lg1352439@gmail.com>
2025-11-22 18:53:57 -0300

    feat: adiciona o método update ao controller (passo 6)

--- src/controller/PetController.java
+++ src/controller/PetController.java
@@ -4,6 +4,7 @@ import model.Pet;
 import service.FileService;
 import service.PetRegister;
 import service.PetSearch;
+import service.PetUpdate;
 
 import java.util.List;
 import java.util.Scanner;
@@ -13,16 +14,18 @@ import static util.MenuUtil.showSearchMenu;
 public class PetController {
     private final PetRegister petRegister = new PetRegister();
     private final PetSearch petSearch = new PetSearch();
+    private final PetUpdate petUpdate = new PetUpdate();
     private final FileService fileService = new FileService();
 
     public void register(Scanner sc) {
         System.out.println("--- Cadastrar Pet ---");
-        Pet pet = petRegister.registerPet(sc);
+        Pet pet = petRegister.registerPet(sc, false);
         fileService.savePetFile(pet);
-        System.out.println("Pet cadastrado e salvo com sucesso!");
+        System.out.println("Pet cadastrado e salvo com sucesso✅!");
     }
 
     public void list() {
+        System.out.println("--- Pets cadastrados ---");
         List<Pet> pets = fileService.readPetFile();
         if (!pets.isEmpty()) {
             int i = 1;
@@ -34,7 +37,7 @@ public class PetController {
         }
     }
 
-    public void search(Scanner sc) {
+    public List<Pet> search(Scanner sc) {
         System.out.println("--- Buscar Pets ---");
 
         List<String> filters = showSearchMenu(sc);
@@ -49,6 +52,20 @@ public class PetController {
             }
         } else {
             System.out.println("Nenhum pet encontrado.");
+        }
+        return results;
+    }
+
+    public void update(Scanner sc) {
+        List<Pet> results = search(sc);
+        if (!results.isEmpty()) {
+            System.out.println("--- Alterar Pet ---");
+            System.out.println("Escolha o número do pet que quer alterar: ");
+
+            List<Pet> pets = petUpdate.updatePet(results, petRegister, sc);
+            fileService.updatePetFile(pets);
+
+            System.out.println("Pet alterado com sucesso✅");
         }
     }
 }
